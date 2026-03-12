4f6c65f66695de709725de114c7a45b726bf2f92
Sonny Brown <lowlifearcade@gmail.com>
2026-03-12 16:41:34 -0700

    feat: code change

--- server/api/prayer.post.ts
+++ server/api/prayer.post.ts
@@ -1,20 +1,25 @@
 export default defineEventHandler(async (event) => {
-    const { title, body } = await readBody(event);
-    const db = useDatabase();
-    const { user } = await getUserSession(event);
+    try {
+        const { title, body } = await readBody(event);
+        const db = useDatabase();
+        const { user } = await getUserSession(event);
 
-    if (!user) {
-        throw createError({ statusCode: 401, message: 'Unauthorized' });
-    }
+        if (!user) {
+            throw createError({ statusCode: 401, message: 'Unauthorized' });
+        }
 
-    const { error, success, lastInsertRowid } = await db.sql`
+        const { error, success, lastInsertRowid } = await db.sql`
         INSERT INTO prayers (title, body, user_id) VALUES (${title}, ${body}, ${user.sub})
     `;
 
-    if (!success) {
-        console.error({ error });
-        throw createError({ message: 'could not add prayer', statusCode: 422 });
-    }
+        if (!success) {
+            console.error({ error });
+            throw createError({ message: 'could not add prayer', statusCode: 422 });
+        }
 
-    return { message: 'success', id: lastInsertRowid, title, body };
+        return { message: 'success', id: lastInsertRowid, title, body };
+    } catch (error) {
+        console.log({ error })
+            throw createError({ message: 'could not add prayer', statusCode: 500 });
+    }
 });
