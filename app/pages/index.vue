020e61c4b1664cd77e5cc56a753a0e1ac66dd167
Sonny Brown <lowlifearcade@gmail.com>
2026-03-13 19:15:31 -0700

    Update form ui

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -41,7 +41,7 @@
             </div>
         </div>
         <ul
-            v-if="loggedIn"
+            v-if="loggedIn && !showAddPrayerForm"
             class="prayers"
         >
             <li v-for="({ title, body, id }, i) in data">
@@ -49,7 +49,7 @@
                     <div class="title">
                         <h3>{{ title }}</h3>
                         <span class="ctx-menu-section">
-                            <Dots
+                            <SvgDots
                                 class="ctx-menu-btn"
                                 alt=""
                                 height="27"
@@ -86,19 +86,7 @@
                     class="add-prayer"
                     @click="showAddPrayerForm = true"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        x="0px"
-                        y="0px"
-                        width="30"
-                        height="30"
-                        viewBox="0 0 50 50"
-                        fill="currentColor"
-                    >
-                        <path
-                            d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z"
-                        ></path>
-                    </svg>
+                    <SvgPlus />
                 </button>
             </li>
         </ul>
@@ -130,25 +118,25 @@
                     name="body"
                 />
             </label>
-            <button
-                class="btn cancel"
-                @click="showAddPrayerForm = false"
-            >
-                Cancel
-            </button>
-            <button
-                class="btn"
-                @click="onAddPrayer"
-            >
-                Add
-            </button>
+            <div class="btns">
+                <button
+                    class="btn form"
+                    @click="onAddPrayer"
+                >
+                    Create
+                </button>
+                <button
+                    class="btn cancel"
+                    @click="showAddPrayerForm = false"
+                >
+                    Cancel
+                </button>
+            </div>
         </form>
     </div>
 </template>
 
 <script setup>
-import Dots from '~/components/Dots.vue';
-
 const { data, pending, refresh } = useFetch('/api/prayers');
 const { loggedIn, user, fetch: refreshSession, clear, ready, openInPopup, session } = useUserSession();
 
@@ -335,15 +323,28 @@ async function onDelete(id) {
         }
     }
 
-    form {
+    .prayer-form {
         display: flex;
         flex-direction: column;
-        gap: 1rem;
+        gap: 3rem;
+        width: 800px;
+        margin-inline: auto;
 
         label {
             h4 {
                 margin-bottom: 1rem;
             }
+        }
+
+        textarea {
+            field-sizing: content;
+            min-height: 50rem;
+            resize: none;
+        }
+
+        .btns {
+            display: grid;
+            gap: 2rem;
         }
     }
 }
