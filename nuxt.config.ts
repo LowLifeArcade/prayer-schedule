af31553cb086eeb3443b65c2098b37846c6522b1
Sonny Brown <lowlifearcade@gmail.com>
2026-04-08 20:40:13 -0700

    feat: code change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,25 +31,7 @@ export default defineNuxtConfig({
         },
         cloudflare: {
             deployConfig: true,
-            // nodeCompat: true,
-            wrangler: {
-                name: 'prayerschedule',
-                compatibility_date: '2026-03-10',
-                compatibility_flags: ['nodejs_compat'],
-                observability: { enabled: true },
-                vars: { ENV: 'prod' },
-                d1_databases: [
-                    {
-                        binding: 'PRAYERS',
-                        database_name: 'prayers-db',
-                        database_id: '0f888000-373c-422f-9920-526b2d20a406',
-                        migrations_dir: 'db/migrations',
-                    },
-                ],
-            },
-            dev: {
-                environment: 'dev',
-            },
+            nodeCompat: true,
         },
         typescript: {
             tsConfig: {
